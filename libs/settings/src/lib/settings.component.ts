ed26d1993447a7e1a131529f093db37740e1a29a
Justin Schwartzenberger <jschwarty@users.noreply.github.com>
2019-10-07 18:03:38 -0700

    add starting app and lib

---
+++ libs/settings/src/lib/settings.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-settings',
+  template: `
+    <p>
+      settings works!
+    </p>
+  `,
+  styles: []
+})
+export class SettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ libs/settings/src/lib/settings.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SettingsComponent } from './settings.component';
+
+
+
+@NgModule({
+  declarations: [SettingsComponent],
+  imports: [
+  ],
+  exports: [SettingsComponent]
+})
+export class SettingsModule { }
